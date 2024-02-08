97ebf67ff8ace2213c9ee3d3db233dec3c6a2587
slince-zero <1711257704@qq.com>
2024-02-08 13:43:42 +0800

    测试严格模式

--- 05-eat-n-split/src/index.js
+++ 05-eat-n-split/src/index.js
@@ -6,6 +6,7 @@ import App from "./App"
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
   <React.StrictMode>
+    {/* 使用严格模式，React会对某些函数组件的一些生命周期方法进行两次调用*/}
     <App />
   </React.StrictMode>
 )
